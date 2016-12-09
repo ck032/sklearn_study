e24a3296df7200b4e23649c7d75df7c21b6835ef
kchen <kchen@chenkai.local>
2016-12-09 11:06:25 +0800

    计算样本权重，平衡y

--- preprocessing/_weights.py
+++ preprocessing/_weights.py
@@ -1,7 +1,8 @@
+# -*- coding:utf-8 -*-
 import numpy as np
 from ..utils.fixes import bincount
 
-
+# 计算权重，权重 = 各个值出现的频率 * 最小值
 def _balance_weights(y):
     """Compute sample weights such that the class distribution of y becomes
        balanced.
@@ -15,6 +16,20 @@ def _balance_weights(y):
     -------
     weights : array-like
         The sample weights.
+
+    y = [2,2,3,5,2,10,20]
+    np.asarray(y)
+    #  array([ 2,  2,  3,  5,  2, 10, 20])
+
+    y = np.searchsorted(np.unique(y),y)
+    # array([0, 0, 1, 2, 0, 3, 4])
+
+    bins
+    # array([3, 1, 1, 1, 1])
+
+    bins.take(y)
+    # array([3, 3, 1, 1, 3, 1, 1])
+
     """
     y = np.asarray(y)
     y = np.searchsorted(np.unique(y), y)
